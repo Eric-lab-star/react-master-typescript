ec90e96501bbf4b5ba97243bf018a3b3bcb36421
Eric <cyon256@icloud.com>
2022-02-26 17:57:11 +0900

    install typscript

---
+++ src/Circle.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Container = styled.div<ContainerProps>`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: ${props=> props.bgColor}
+`;
+
+interface ContainerProps {
+    bgColor: string;
+}
+
+
+
+
+const Circle = ({bgColor}:ContainerProps) =>{
+    return (
+        <Container bgColor={bgColor}/>
+    );
+}
+
+export default Circle;
