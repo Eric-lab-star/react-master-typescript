ea24fe661dc12f5efe102f5b659bbec91ed127c1
Eric Kim <cyon256@icloud.com>
2022-03-02 14:21:58 +0900

    extend styled-component

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme ={
+    bgColor: "yellow",
+    textColor: "black",
+    btnColor: "tomato",
+}
+
+export const darkTheme: DefaultTheme = {
+
+    bgColor: "black",
+    textColor: "white",
+    btnColor: "red"
+    
+}
